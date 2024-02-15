168bada7ac4f1c46c83087ae0efef2a7cdaf024f
Dharmendra Kanjaria <112968720+idjey@users.noreply.github.com>
2024-02-15 09:57:37 -0500

    Create config.py

---
+++ scripts/config.py
@@ -0,0 +1,7 @@
+config = {
+    "webdav_url": "https://hjf-bdw-stage.lkcompliant.net/_webdav/Transformed_Data/@files/",
+    "local_folder_path": r"test_files",
+    "log_folder": r"logs",
+    "schedule_time": "02:00",
+    "transformation_scripts": ["test_script\transformation\test.py"]
+}
