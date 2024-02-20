2cf92e1eac052bc6b8492cc030fe2352d1727a6d
Dharmendra Kanjaria <112968720+idjey@users.noreply.github.com>
2024-02-20 11:27:02 -0500

    Update config_manager.py

--- scripts/config_manager.py
+++ scripts/config_manager.py
@@ -8,19 +8,6 @@ class ConfigManager:
         self.load_default_config(default_config_path)
         self.override_with_env_variables()
 
-    # def load_default_config(self, path):
-    #     try:
-    #         with open(path, 'r') as config_file:
-    #             self.config = json.load(config_file)
-    #     except FileNotFoundError:
-    #         print(f"Warning: Default configuration file {path} not found.")
-
-    # def override_with_env_variables(self):
-    #     for key, value in os.environ.items():
-    #         if key.startswith('APP_'):
-    #             config_key = key[4:]  # Remove 'APP_' prefix
-    #             self.config[config_key] = value
-
     def load_default_config(self, path):
         try:
             with open(path, 'r') as config_file:
