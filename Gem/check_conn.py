4a6ce1bc3814c12d30ecfb92956e609cbdac46cb
Dharmendra Kanjaria <112968720+idjey@users.noreply.github.com>
2024-02-28 11:34:35 -0500

    Update check_conn.py

--- Gem/check_conn.py
+++ Gem/check_conn.py
@@ -1,14 +1,18 @@
 import requests
 import subprocess
 import logging
-from config_manager import ConfigManager  # Assuming this is where your webdav_url is configured
+from config_manager import ConfigManager
+
+custom_certificate_path = r'C:\Users\dkanjaria\Downloads\ZscalerRootCertificate-2048-SHA256.crt'
+
 
 # Initialize logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 def check_webdav_connection(url):
-    """Check connectivity to the WebDAV server."""
+    """Check connectivity to the Webdev server."""
     try:
         response = requests.head(url)
         # 200 OK or 401 Unauthorized indicates the URL is reachable
@@ -16,11 +20,13 @@ def check_webdav_connection(url):
             logger.info(f"Connection to {url} successful.")
             return True
         else:
-            logger.error(f"Connection to {url} failed with status code: {response.status_code}.")
+            logger.error(f"Connection to {url} failed with status code: {
+                         response.status_code}.")
             return False
     except requests.exceptions.RequestException as e:
         logger.error(f"Failed to connect to {url}. Error: {e}")
         return False
+
 
 def run_main():
     """Run the main.py script."""
@@ -30,11 +36,12 @@ def run_main():
     except subprocess.CalledProcessError as e:
         logger.error(f"main.py execution failed: {e}")
 
+
 if __name__ == "__main__":
     config = ConfigManager(default_config_path='config.json')
-    webdav_url = config.get('webdav_url')
+    webdev_url = config.get('webdev_url')
 
-    if webdav_url and check_webdav_connection(webdav_url):
+    if webdev_url and check_webdav_connection(webdev_url):
         run_main()
     else:
         logger.error("Skipping main.py execution due to connectivity issues.")
